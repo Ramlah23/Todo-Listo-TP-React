e99f0fc311822891162dd74c05e51bd935712163
Cinthia <cinthia_ramlah@hotmail.com>
2024-07-18 20:56:14 -0300

    se agrego box de tareas completas incompletas y todas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-
-
-import { useState } from 'react';
-import TodoForm from './components/form/Form';
-import TodoList from './components/todolist/todolist';
-import { Footer } from './components/footer/footer.jsx';
-import { Container, Typography } from '@mui/material';
+import { useState } from "react";
+import TodoForm from "./components/form/Form";
+import TodoList from "./components/todolist/todolist";
+import { Footer } from "./components/footer/footer.jsx";
+import { Container, Typography, Box } from "@mui/material";
 import "./App.css";
 
 const App = () => {
@@ -15,32 +13,58 @@ const App = () => {
   };
 
   const toggleComplete = (id) => {
-    setTodos(todos.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo));
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
   };
 
   const removeTodo = (id) => {
-    setTodos(todos.filter(todo => todo.id !== id));
+    setTodos(todos.filter((todo) => todo.id !== id));
   };
 
   const editTodo = (id) => {
-    const newTask = prompt('Edita tu tarea:');
+    const newTask = prompt("Edita tu tarea:");
     if (newTask) {
-      setTodos(todos.map(todo => todo.id === id ? { ...todo, task: newTask } : todo));
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, task: newTask } : todo
+        )
+      );
     }
   };
 
+  const totalTodos = todos.length;
+  const completedTodos = todos.filter((todo) => todo.completed).length;
+  const incompleteTodos = todos.length - completedTodos;
+
   return (
     <div className="App">
-    <Container>
-      <Typography variant="h4" gutterBottom>
-        Todo Listo
-      </Typography>
-      <TodoForm addTodo={addTodo} />
-      <TodoList todos={todos} toggleComplete={toggleComplete} removeTodo={removeTodo} editTodo={editTodo} />
-    </Container>
-    <Footer />
-    </div>
+      <Container>
+        <Typography variant="h4" gutterBottom>
+          Todo Listo
+        </Typography>
+        <TodoForm addTodo={addTodo} />
+        <TodoList
+          todos={todos}
+          toggleComplete={toggleComplete}
+          removeTodo={removeTodo}
+          editTodo={editTodo}
+        />
+        <Box mt={2}>
+          <Typography variant="body1">Total de Tareas: {totalTodos}</Typography>
+          <Typography variant="body1">
+            Tareas Completas: {completedTodos}
+          </Typography>
+          <Typography variant="body1">
+            Tareas Incompletas: {incompleteTodos}
+          </Typography>
+        </Box>
+      </Container>
 
+      <Footer />
+    </div>
   );
 };
 
